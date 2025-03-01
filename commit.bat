738aa9bd83e3187309012ce938d23dd126558e58
chinh <danhchinh2024@gmail.com>
2025-03-02 03:43:17 +0700

    Auto commit: Sun 03/02/2025  3:43:17.31

--- commit.bat
+++ commit.bat
@@ -1,18 +1,18 @@
 @echo off
-echo Đang kiểm tra trạng thái Git...
-git status
-
-echo Đang thêm các thay đổi vào staging area...
+echo git add...
 git add .
 
+echo git config --global user.email "danhchinh2024@gmail.com"
 git config --global user.email "danhchinh2024@gmail.com"
-git config --global user.name "chinh.com"
 
-echo Đang commit các thay đổi...
+echo git config --global user.name "chinh"
+git config --global user.name "chinh"
+
+echo git commit -m "Auto commit: %date% %time%"
 git commit -m "Auto commit: %date% %time%"
 
-echo Đang push các thay đổi lên GitHub...
+echo git push origin main
 git push origin main
 
-echo Hoàn thành!
+echo Done!
 pause
