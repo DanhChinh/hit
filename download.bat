b807c14df1f7bf994db7be0b9ba7e45b5931a628
chinh <danhchinh2024@gmail.com>
2025-03-02 04:34:37 +0700

    Auto commit: Sun 03/02/2025  4:34:37.36

--- download.bat
+++ download.bat
@@ -1,18 +1,25 @@
 @echo off
-echo Đang tải và cài đặt Python...
+echo Download Python...
 curl -o python_installer.exe https://www.python.org/ftp/python/3.13.2/python-3.13.2-amd64.exe
 start /wait python_installer.exe /quiet InstallAllUsers=1 PrependPath=1
+del python_installer.exe 
 
-echo Đang tải và cài đặt Git...
-curl -o git_installer.exe https://github.com/git-for-windows/git/releases/download/v2.48.1.windows.1/Git-2.48.1-64-bit.exe
-start /wait git_installer.exe /VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS="icons,ext\reg\shellhere,assoc,assoc_sh"
+:: Tải Git từ trang chính thức
+echo Download Git...
+powershell -Command "Invoke-WebRequest -Uri https://github.com/git-for-windows/git/releases/download/v2.48.1.windows.1/Git-2.48.1-64-bit.exe -OutFile Git-installer.exe"
 
-echo Đang clone dự án từ GitHub...
+:: Cài đặt Git
+start /wait Git-installer.exe /SILENT /NORESTART
+
+:: Xóa tệp cài đặt sau khi cài đặt xong
+echo Dọn dẹp...
+del Git-installer.exe
+echo clone https://github.com/DanhChinh/hit.git
 git clone https://github.com/DanhChinh/hit.git
 
-echo install module
+echo pip install -r requirement.txt
 cd ".\hit\AI"
 pip install -r requirement.txt
 
-echo Tất cả các tác vụ đã hoàn thành!
+echo Done!
 pause
