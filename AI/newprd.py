71e451bca355dd977dedc9608128c76af1a314d9
chinh <danhchinh2024@gmail.com>
2025-03-08 19:15:02 +0700

    Auto commit: Sat 03/08/2025 19:15:02.62

--- AI/newprd.py
+++ AI/newprd.py
@@ -5,7 +5,19 @@ from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
+from scipy.stats import linregress
+def get_correlation(data):
+    """
+    Tính hệ số tương quan r giữa chỉ số và dữ liệu.
+    Nếu r > 0.5 → Xu hướng tăng.
+    Nếu r < -0.5 → Xu hướng giảm.
+    """
+    if len(data) < 2:
+        return 0  # Không đủ dữ liệu để tính toán
 
+    x = np.arange(len(data))  # Chỉ số x: [0,1,2,...]
+    slope, intercept, r_value, p_value, std_err = linregress(x, data)
+    return r_value
 def makeData():
     df = readTable()
     l = len(df)
@@ -43,17 +55,23 @@ def findBestModel(data_train, data_test, label_train, label_test):
         model.fit(data_train, label_train)
         predictions = model.predict(data_test)
         predictions =  le.inverse_transform(predictions)#.tolist()
-        slopes = []
+        correlation_arr = []
         for i in range(len(label_test)):
-            slope = compare_labels(predictions[i], label_test[i])
-            slopes.append(slope)
-        print(slopes)
-        return 1
+            correlation = compare_labels(predictions[i], label_test[i])
+            correlation_arr.append(correlation)
+        correlation_arr = np.cumsum(correlation_arr)
+        score = get_correlation(correlation_arr)
+        if score> bestScore:
+            bestScore = score
+            bestModel = model
+    return bestModel 
 
 def predict():
     data, label, xpredict = makeData()
     data_train, data_test, label_train, label_test = split_data(data, label, 10)
     bestModel = findBestModel(data_train, data_test, label_train, label_test)
+    predict = bestModel.predict([xpredict])
+    print(predict)
 
 predict()
 
