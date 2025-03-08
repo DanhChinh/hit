e43560b7171a1208c968e8be509bd8a655b80b13
chinh <danhchinh2024@gmail.com>
2025-03-09 06:04:23 +0700

    Auto commit: Sun 03/09/2025  6:04:23.20

--- AI/run.py
+++
@@ -1,8 +0,0 @@
-from models import makePredict
-from db import df_get_hsft
-
-last_sid = 1869907
-
-for sid in range(last_sid-330, last_sid):
-    predictions = makePredict(sid)
-    print(f"sid: {sid}, predictions: {predictions}")
