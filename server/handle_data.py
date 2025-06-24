fa4fa4263fa360b77bf68ad8046b4f3af37a0a9d
chinh <danhchinh2024@gmail.com>
2025-06-24 18:43:26 +0700

    add

--- server/handle_data.py
+++ server/handle_data.py
@@ -44,7 +44,7 @@ def handle_progress(progress, isEnd = True):
     #     avg_bc = sum_bc[eid] / count[eid]
     #     avg_v = sum_v[eid] / count[eid]
     #     print(f"[eid={eid}] Trung bình bc: {avg_bc}, Trung bình v: {avg_v}")
-    return int(sum_bc[1] / 3), int(sum_v[1] / 3), int(sum_bc[2] / 3), int(sum_v[2] / 3)
+    return int(sum_bc[1] / 3), int(sum_v[1] / 3000000), int(sum_bc[2] / 3), int(sum_v[2] / 3000000)
 
 
 
@@ -54,4 +54,18 @@ for index, row in df.iterrows():
     avg_bc1, avg_v1, avg_bc2, avg_v2 = handle_progress(row['progress'])
     if avg_bc1:
         data_perfect.append([avg_bc1, avg_v1, avg_bc2, avg_v2])
-print(numpy.array(data_perfect))
+
+
+data = numpy.array(data_perfect)
+
+
+
+import numpy as np
+from sklearn.preprocessing import RobustScaler
+
+scaler = RobustScaler()
+data_scaled = scaler.fit_transform(data)
+data_rounded = np.round(data_scaled, 1)
+
+for i in data_rounded:
+    print(i)
